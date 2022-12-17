42cfd253c5523aea0573188673576bee3fed88fb
Rahul_Kujur <56760617+rahul8969@users.noreply.github.com>
2022-12-17 13:17:47 +0530

    Add files via upload

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,12 +17,13 @@ export class HomeComponent implements OnInit {
   selectedBrand:any;
   cartList:Array<any>=[];
   total=0;
-  prices=[{price:"All"},{price:"200 - 500"},{price:"1000 - 5000"},{price:"5000 - 25000"},{price:"25000 - 100000"}];
+  prices=[{price:"All"},{price:"1 - 500"},{price:"500 - 999"},{price:"1000 - 5000"},{price:"5000 - 25000"},{price:"25000 - 100000"}];
   ratings=[{rating:"All"},{rating:1},{rating:2},{rating:3},{rating:4},{rating:5}];
   discounts=[{discount:"All"},{discount:5},{discount:10},{discount:15},{discount:20},{discount:25},{discount:30},{discount:35},{discount:40},{discount:45},{discount:50}];
   priceRange:any;
   ratingRange:any;
   discountRange:any;
+  quantity:any;
 
   slideConfig = {  
     "slidesToShow": 3,  
@@ -67,7 +68,9 @@ export class HomeComponent implements OnInit {
       }
     })
     console.log("brandlist", this.brandList)
+    this.brandList.unshift("All Brands");
     console.log("categories", this.categoryList)
+    this.categoryList.unshift("All Categories");
     // ends
     this.sortingProductList(this.allProductList);
   }
@@ -76,13 +79,15 @@ export class HomeComponent implements OnInit {
     let demoList: Array<any> = [];
     let demoList2: Array<any> = [];
     this.categoryList.map(category => {
-      data.map(items => {
-        if (category == items.category) {
-          demoList.push(items);
-        }
-      })
-      if(demoList.length>0)demoList2.push({ type: category, data: demoList })
-      demoList = [];
+      if(category!="All Categories"){
+        data.map(items => {
+          if (category == items.category) {
+            demoList.push(items);
+          }
+        })
+        if(demoList.length>0)demoList2.push({ type: category, data: demoList })
+        demoList = [];
+      }
     })
     this.productByCategory = demoList2;
     console.log("product list by category", this.productByCategory);
@@ -93,12 +98,16 @@ export class HomeComponent implements OnInit {
     this.selectedCategory=data;
     this.productList=this.allProductList;
     let selectedProducts:Array<any>=[];
-    this.productList.map(items=>{
-      if(items.category==data){
-        selectedProducts.push(items);
-      }
-    })
-    this.sortingProductList(selectedProducts);
+    if(data=="All Categories"){
+      this.sortingProductList(this.allProductList);
+    }else{
+      this.productList.map(items=>{
+        if(items.category==data){
+          selectedProducts.push(items);
+        }
+      })
+      this.sortingProductList(selectedProducts);
+    }
   }
 
   filterByBrands(data:any){
@@ -106,23 +115,28 @@ export class HomeComponent implements OnInit {
     this.selectedCategory=data;
     this.productList=this.allProductList;
     let selectedProducts:Array<any>=[];
-    this.productList.map(items=>{
-      if(items.brand==data){
-        selectedProducts.push(items);
-      }
-    })
-    this.sortingProductList(selectedProducts);
+    if(data=="All Brands"){
+      this.sortingProductList(this.allProductList);
+    }else{
+      this.productList.map(items=>{
+        if(items.brand==data){
+          selectedProducts.push(items);
+        }
+      })
+      this.sortingProductList(selectedProducts);
+    }
   }
   filterByPrice(data:any){
     console.log(data);
     this.priceRange=data;
+    let range=this.priceRange.split(' - ',2);
     this.productList=this.allProductList;
     let selectedProducts:Array<any>=[];
     if(data=="All"){
       this.sortingProductList(this.allProductList);
     }else{
       this.productList.map(items=>{
-        if(items.price==data){
+        if(items.price>=range[0] && items.price<=range[1]){
           selectedProducts.push(items);
         }
       })
@@ -166,22 +180,66 @@ export class HomeComponent implements OnInit {
   }
 
   addToCart(data:any){
-    this.cartList.push(data);
-    console.log("cart-list",this.cartList)
+    if(this.cartList.length>0){
+      this.cartList.map(ele=>{
+        if(ele.product.id==data.id){
+          ele.quantity+=1;
+        }else{
+          this.cartList.push({product:data,price:data.price,quantity:1});
+        }
+      })
+    }else{
+      this.cartList.push({product:data,price:data.price,quantity:1});
+    }
+    
+  }
+
+  removeFromCart(data:any){
+    this.cartList.map(items=>{
+      if(data.product.id==items.product.id){
+        this.cartList.splice(this.cartList.indexOf(items.product.id), 1);
+      }
+    })
+    this.goToCart();
   }
 
   goToCart(data?:any){
+    this.total=0;
     this.showProductList=false;
-    // this.cartList.push(data);
     this.cartList.map(items=>{
       this.total+=items.price;
     })
-    console.log("buy-list",this.cartList)
+  }
+
+  buyProduct(data:any){
+    let foundProduct=false;
+    if(this.cartList.length>0){
+      this.cartList.map(items=>{
+        if(data.id==items.product.id){
+          foundProduct=true;
+        }
+      })
+      if(!foundProduct)this.cartList.push({product:data,price:data.price,quantity:1})
+    }else{
+      this.cartList.push({product:data,price:data.price,quantity:1});
+    }
+    this.goToCart();
   }
 
   backToHome(){
     this.showProductList=true;
     this.productList=this.allProductList;
+  }
+
+  getQuantity(data:any,items:any){
+    console.log("quantity",data,items)
+    this.cartList.map(ele=>{
+      if(items['product']['id']==ele.product.id){
+        if(data=="add"){ele.quantity++;ele.price=ele.quantity*ele.product.price}
+        else{ele.quantity--;ele.price=ele.quantity*ele.product.price}
+      }
+    })
+    this.goToCart();
   }
 
 }
